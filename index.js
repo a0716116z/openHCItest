d324f3e603c6d4926b97ae8b3e8b9ea2304af028
a0716116z <65119444+a0716116z@users.noreply.github.com>
2022-08-28 00:28:02 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log("Loading");
+let leftsec=300;
+function startTimer(duration, display) {
+    let timer = duration;
+    setInterval(function () {
+        // minutes = parseInt(timer / 60, 10);
+        // seconds = parseInt(timer % 60, 10);
+
+        // minutes = minutes < 10 ? "0" + minutes : minutes;
+        // seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = timer;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+window.onload = function () {
+    const gamestart = document
+      .querySelector(".gameStart")
+      .addEventListener("click", function () { 
+         display = document.querySelector('#timeleft');  
+    startTimer(leftsec, display);
+      document.getElementById("gameStart").classList.add("displaynone")
+
+      });
+   
+    };
